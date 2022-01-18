30baff0f8c9a71edc7e7c9838f05d15c5e3d7cd7
chaplinrus <chaplinrus@gmail.com>
2022-01-18 10:54:25 +0300

    First task

--- app/src/main/java/com/example/myfirsttask/MainActivity.java
+++ app/src/main/java/com/example/myfirsttask/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.myfirsttask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text = findViewById(R.id.text);
+        Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        TextView text3 = findViewById(R.id.text3);
+        EditText editText = findViewById(R.id.edit_text);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                text.setText("Cпасибо что нажали");
+            }
+        });
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text3.setText(editText.getText());
+            }
+        });
     }
 }
